b9622fd2f2e1f43cfe9f8ef364d7bdd0233f27e9
srivignesh <srivignesh123xworkz@gmail.com>
2022-03-01 17:43:02 +0530

    STRING ADD AND DISPLAY METHODS IN JAVA

---
+++ JAVAPROGRAMS/StringAddDisplayTester.java
@@ -0,0 +1,44 @@
+class StringAddDisplayTester{
+
+public static void main(String[] args){
+
+
+
+StringAddDisplay  stringAddDisplay=new StringAddDisplay();
+stringAddDisplay.addName(" VIGNESH ");
+stringAddDisplay.addName(" VIGNESH IS SWIMMER");
+stringAddDisplay.addName(" VIGNESH IS SWIMMER AND BRONZE MEDALISTS");
+stringAddDisplay.addName(" KILL NEGATITIVITY BULID UP POSITIVITY");
+stringAddDisplay.addName(" LIFE IS JOURNEY FROM SOURCE TO DESTINATION WHERE WE A LOT OF NEAGTIVES AND POSTIVES ,ADD POSITIVE TO NEGATIVE ,IT WILL TURN UR WAY");
+stringAddDisplay.addName(" ALLWAYS BE POSTIVE");
+stringAddDisplay.addName("  AM FROM NELLORE");
+stringAddDisplay.addName(" JAVA IS AN OBJECT ORIENTD PROGRAMMING LANGUAGE");
+stringAddDisplay.addName(" STRING IS NOTHING BUT SOME GROUP OF CHARECTERS ");
+stringAddDisplay.addName(" String are immutable");
+stringAddDisplay.addName(" my favourite game is swimming");
+stringAddDisplay.addName(" CONSTRUCTOR IS NOTHING BUT A SIMILAR METHOD ITS A BLOCK OF CODE OR A STATEMENTS");
+stringAddDisplay.addName(" DEFAULT CONSTRUCTOR AND PARAMETERISED CONSTRUCTOR");
+stringAddDisplay.addName(" STRING ARE IMMUTABLE");
+stringAddDisplay.addName(" CLASS NAME WE CALL IT AS NON-PRIMITIVE CUSTOM DATA TYPE");
+stringAddDisplay.addName(" XWORKZ IS THE BEST SOFTWARE DEVELOPMENT COMPANY AND TRAINING");
+
+stringAddDisplay.displayNames();
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+}
