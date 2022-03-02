5a945ea34107deabc59c54524361d386c4ba4ff1
srivignesh123xworkz@gmail.com <srivignesh123xworkz@gmail.com>
2022-03-02 23:45:55 +0530

    adding,displaying (2-3-22)

---
+++ JAVAPROGRAMS/StringMethodsTester.java
@@ -0,0 +1,32 @@
+class StringMethodsTester{
+
+public static void main(String[] medicines){
+
+StringMethods stringMethods=new StringMethods();
+
+
+stringMethods.addMedicines("ALBENDOZOL");
+
+stringMethods.find("ALBENDOZOL");
+stringMethods.find("PARACETMOL");
+
+
+
+//stringMethods.find("ALBENDOZO");
+//stringMethods.find("ZINTAC");
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+}
+
