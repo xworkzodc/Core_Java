95b51bbf1632c71f251382877e8467eeeaba2ce3
srivignesh123xworkz@gmail.com <srivignesh123xworkz@gmail.com>
2022-04-11 13:53:13 +0530

    Adding Exception files (11-4-22)

---
+++ Exceptions/src/com/xworkz/exceptions/NameServiceRunner.java
@@ -0,0 +1,27 @@
+package com.xworkz.exceptions;
+import com.xworkz.exceptions.NameService;
+public class NameServiceRunner {
+	
+	
+	public static void main(String[] args)
+	{
+		NameService nameService=new NameService();
+		nameService.add("XWORKZ");
+		try {
+			nameService.add("INFOSYS");
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		nameService.add("Google");
+		nameService.add("WIPRO");
+		nameService.add("TCS@#");
+		try {
+			nameService.add("SYNCHRONOUS");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
